efae862d446d57095fba7355e98261e6b9d62560
Test team 1 <tddror5railsudemytest1@gmail.com>
2017-11-27 01:22:07 +0000

    Add user and stock association plus views

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search' 
+  resources :user_stocks, only: [:create, :destroy]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
