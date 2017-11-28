0bfe988066d0ae589e647ef1ef3078f71b898f3b
Test team 1 <tddror5railsudemytest1@gmail.com>
2017-11-28 17:40:08 +0000

    Complete finance tracker app

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search' 
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
   get 'my_friends', to: 'users#my_friends'
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
